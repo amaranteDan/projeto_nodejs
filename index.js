1cbfd81ec98ad2546205a6350104467d3459b0a1
Daniel Amarante <danz82158@gmail.com>
2023-05-30 12:51:05 -0300

    Atualização Utils/validator.js

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 //Criando um Servidor http
-const express = require('express')
-const consign = require('consign')
-const bodyParser = require('body-parser')
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
+let app = express();
 
-let app = express()
-/**Usando o body-parser -  */
-app.use(bodyParser.urlencoded({ extended: false })) /**Fazendo a codificação */
-app.use(bodyParser.json())/**Todos os dados que pegar converte em json */
-/**
- * No consign incluio o arquivo de rotas e abre no (app)
- */
-consign().include('/routes/').into(app)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(expressValidator());
 
+consign().include('routes').include('utils').into(app);
 
+app.listen(3000, '127.0.0.1', ()=>{
 
-app.listen(9000, '127.0.0.1', ()=> {
-    console.log('servidor rodando')
-})
+    console.log('servidor rodando!');
+
+});
